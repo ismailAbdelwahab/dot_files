9f7bcea4a807ba36b8f2f7272a7428be9307e17d
ismailAbdelwahab <is.abdelwahab@gmail.com>
2019-11-24 16:49:12 +0100

    Adding script v1

---
+++ setup_new_linux.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Define colors...
+RED=`tput bold && tput setaf 1`
+GREEN=`tput bold && tput setaf 2`
+YELLOW=`tput bold && tput setaf 3`
+BLUE=`tput bold && tput setaf 4`
+NC=`tput sgr0`
+
+function RED(){
+	echo -e "\n${RED}${1}${NC}"
+}
+function GREEN(){
+	echo -e "\n${GREEN}${1}${NC}"
+}
+function YELLOW(){
+	echo -e "\n${YELLOW}${1}${NC}"
+}
+function BLUE(){
+	echo -e "\n${BLUE}${1}${NC}"
+}
+
+# Testing if root...
+if [ $UID -ne 0 ]
+then
+	RED "You must run this script as root!" && echo
+	exit
+fi
+
+
+BLUE "Updating repositories..."
+sudo apt update
+
+BLUE "Installing git..."
+sudo apt install -y git
+
+BLUE "Installing Sublime Text..." # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
+
+BLUE "Installing pip..."
+sudo apt-get install -y python-pip
+
+BLUE "Installing python-requests..."
+pip install requests
+
+BLUE "Installing Python pwntools..."
+sudo pip install pwntools
+
+BLUE "Installing nmap..."
+sudo apt-get install -y nmap
+
+BLUE "Installing curl..."
+sudo apt-get install -y curl
+
+BLUE "Installing Wireshark..."
+sudo apt-get install -y wireshark
+
