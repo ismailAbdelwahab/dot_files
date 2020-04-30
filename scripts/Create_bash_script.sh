19a50dd09e474e51c53add73215d4f7efddb88ca
ismailAbdelwahab <44019403+ismailAbdelwahab@users.noreply.github.com>
2020-04-30 18:51:30 +0200

    Update Create_bash_script.sh
     Setting the header to be more compact

--- scripts/Create_bash_script.sh
+++ scripts/Create_bash_script.sh
@@ -52,24 +52,20 @@ chmod +x ${FILE}
 #Fill file with python requirements:
 echo "#!/bin/bash" > ${FILE}
 echo -e "
+################ Colors #######################
 RED=\`tput bold && tput setaf 1\`
 GREEN=\`tput bold && tput setaf 2\`
 YELLOW=\`tput bold && tput setaf 3\`
 BLUE=\`tput bold && tput setaf 4\`
 NC=\`tput sgr0\`
+function RED(){ echo -e "\${RED}\${1}\${NC}" ;}
+function GREEN(){ echo -e "\${GREEN}\${1}\${NC}" ;}
+function YELLOW(){  echo -e "\${YELLOW}\${1}\${NC}" ;}
+function BLUE(){ echo -e "\${BLUE}$\{1}\${NC}" ;}
+##############################################
+################# Functions ##################
 
-function RED(){
-        echo -e "\${RED}\${1}\${NC}" 
-}
-function GREEN(){
-        echo -e "\${GREEN}\${1}\${NC}"
-}
-function YELLOW(){
-        echo -e "\${YELLOW}\${1}\${NC}"
-}
-function BLUE(){
-        echo -e "\${BLUE}$\{1}\${NC}"
-}
+################## Script ####################
 " >> ${FILE}
 
 #Done.
