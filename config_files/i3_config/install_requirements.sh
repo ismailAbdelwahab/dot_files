7254c82d05ae0e67dc6d46ee4f88a6b937b6ab64
ismailAbdelwahab <is.abdelwahab@gmail.com>
2022-02-01 17:53:56 +0100

    {i3} Updating config files.

--- config_files/i3_config/install_requirements.sh
+++ config_files/i3_config/install_requirements.sh
@@ -7,3 +7,12 @@ sudo apt install -y i3
 sudo add-apt-repository ppa:regolith-linux/release
 sudo apt update -y
 sudo apt install -y i3-gaps
+
+# compton
+sudo apt install compton
+
+# feh
+sudo apt install feh
+
+# lxappearance
+sudo apt install lxappearance
