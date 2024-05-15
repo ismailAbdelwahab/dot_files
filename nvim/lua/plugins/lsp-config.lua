e3a054ba1f2276e5c630d483c139803e9593774b
Ismail ABDEL WAHAB <is.abdelwahab@gmail.com>
2024-05-15 18:28:57 +0200

    [nvim] Adding completions pluggins.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -30,23 +30,26 @@ return {
     { -- Make nvim use those LSP servers
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({})
-            lspconfig.marksman.setup({})
-            lspconfig.bashls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.basedpyright.setup({})
-            lspconfig.jsonls.setup({})
-            lspconfig.texlab.setup({})
-            lspconfig.dockerls.setup({})
-            lspconfig.autotools_ls.setup({})
-            lspconfig.html.setup({})
-            lspconfig.phpactor.setup({})
-            lspconfig.sqls.setup({})
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({capabilities=capabilities})
+            lspconfig.marksman.setup({capabilities=capabilities})
+            lspconfig.bashls.setup({capabilities=capabilities})
+            lspconfig.clangd.setup({capabilities=capabilities})
+            lspconfig.basedpyright.setup({capabilities=capabilities})
+            lspconfig.jsonls.setup({capabilities=capabilities})
+            lspconfig.texlab.setup({capabilities=capabilities})
+            lspconfig.dockerls.setup({capabilities=capabilities})
+            lspconfig.autotools_ls.setup({capabilities=capabilities})
+            lspconfig.html.setup({capabilities=capabilities})
+            lspconfig.phpactor.setup({capabilities=capabilities})
+            lspconfig.sqls.setup({capabilities=capabilities})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}) -- Know more about what hovered
+            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {}) -- Jump to code definition
+            vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {}) -- Jump to code references
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {}) -- Act on code under cursor
         end
     },
 }
