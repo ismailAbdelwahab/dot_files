d06e5eb4617d4c9c01a2222793f200927a0f930c
Ismaïl ABDEL WAHAB <is.abdelwahab@gmail.com>
2024-05-23 19:08:05 +0200

    [nvim] Updating oil and telescope's shortcuts

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,19 @@
 return {
-    { -- Telescope itself
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.6",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    { -- ui select extention
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown({}) } },
-            })
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+	{ -- Telescope itself
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({})
+		end,
+	},
+	{ -- ui select extention
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown({}) } },
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- .config/nvim/lua/shortcuts.lua
+++ .config/nvim/lua/shortcuts.lua
@@ -1,44 +1,63 @@
--- Neo-tree
+--------------
+-- Neo-tree --
+--------------
 vim.keymap.set("n", "<C-f>", ":Neotree toggle right<CR>", { desc = "Open the file browser" })
 
--- Lsp-config
+----------------
+-- Lsp-config --
+----------------
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Know more about what is hovered" })
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Jump to code definition" })
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Jump to code references" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Act on code under cursor" })
 
--- None-ls
+-------------
+-- None-ls --
+-------------
 vim.keymap.set("n", "<Leader>fc", vim.lsp.buf.format, { desc = "Format code" })
 
--- Tmux
+----------
+-- Tmux --
+----------
 vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>", { desc = "Move focus to LEFT window" })
 vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>", { desc = "Move focus to DOWN window" })
 vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>", { desc = "Move focus to UP window" })
 vim.keymap.set("n", "C-l", ":TmuxNavigateRight<CR>", { desc = "Move focus to RIGHT window" })
 
--- Telescope
+---------------
+-- Telescope --
+---------------
+--- Find file
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find file" })
+vim.keymap.set("n", "<leader>fd", ":cd %:p:h<CR>:Telescope find_files<CR>", { desc = "Find file (based on the current's file working directory)" })
+--- grep
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Find grep" })
+vim.keymap.set("n", "<leader>/", ":Telescope current_buffer_fuzzy_find<CR>", { desc = "'grep' in the current file" })
 
--- Oil
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<Leader>o", ":Oil --float %:p:h<CR>", { desc = "Open oil on the parent directory of the current's file in the buffer" })
+---------
+-- Oil --
+---------
+vim.keymap.set("n", "<Leader>o", ":Oil --float %:p:h<CR>", { desc = "Open oil" })
 
--- Tree sitter
--- Check the "lua/plugins/treesitter.lua" file
+-----------
+-- Neorg --
+-----------
+--- Open / Exit neorg
+vim.keymap.set("n", "<leader>ni", ":Neorg index<CR>", { desc = "Open Neorg Index" })
+vim.keymap.set("n", "<leader>nr", ":Neorg return<CR>", { desc = "Exit Neorg using the 'return' command" })
+--- Generate Metadata && Summary && TOC
+vim.keymap.set("n", "<leader>nm", ":Neorg inject-metadata<CR>", { desc = "Inject metadata to the current note" })
+vim.keymap.set("n", "<leader>nws", ":Neorg generate-workspace-summary<CR>", { desc = "Generate the current's workspace summary" })
+vim.keymap.set("n", "<leader>nt", ":Neorg toc<CR>", { desc = "Generate TOC" })
+--- Journal && Calendar
+vim.keymap.set("n", "<leader>nj", ":Neorg journal<CR>", { desc = "Open Neorg journal" })
+vim.keymap.set("n", "<leader>nc", ":Neorg journal custom<CR>", { desc = "Open Neorg calendar" })
+--- Toggle concealer
+vim.keymap.set("n", "<leader>nc", ":Neorg toggle-concealer<CR>", { desc = "Toggles Neorg concealer mode" })
 
--- Completions
--- Check the "lua/plugins/completions.lua" file
-
--- Treesitter (code selection)
--- Check the "lua/plugins/treesitter.lua" file
-
--- Neorg
-vim.keymap.set('n', '<leader>ni', ':Neorg index<CR>', { desc = "Open Neorg Index" })
-vim.keymap.set('n', '<leader>nr', ':Neorg return<CR>', { desc = "Exit Neorg using the 'return' command" })
-vim.keymap.set('n', '<leader>nm', ':Neorg inject-metadata<CR>', { desc = "Inject metadata to the current note" })
-vim.keymap.set('n', '<leader>nj', ':Neorg journal<CR>', { desc = "Open Neorg journal" })
-vim.keymap.set('n', '<leader>nc', ':Neorg journal custom<CR>', { desc = "Open Neorg calendar" })
-vim.keymap.set('n', '<leader>nc', ':Neorg toggle-concealer<CR>', { desc = "Toggles Neorg concealer mode" })
-vim.keymap.set('n', '<leader>nws', ':Neorg generate-workspace-summary<CR>', { desc = "Generate the current's workspace summary" })
-vim.keymap.set('n', '<leader>nt', ':Neorg toc<CR>', { desc = "Generate TOC" })
+-------------------------
+-- Dedicated shortcuts --
+-------------------------
+-- Tree sitter                  => Check the "lua/plugins/treesitter.lua" file
+-- Completions                  => Check the "lua/plugins/completions.lua" file
+-- Treesitter (code selection)  => Check the "lua/plugins/treesitter.lua" file
