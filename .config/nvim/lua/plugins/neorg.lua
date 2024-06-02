0e71664e705477b2eb149c2d81b38dc9d758c864
Ismaïl ABDEL WAHAB <is.abdelwahab@gmail.com>
2024-06-02 14:23:16 +0200

    [Neorg] adding todo item shortcut management

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -13,6 +13,28 @@ return {
 			require("neorg").setup({
 				load = {
 					["core.defaults"] = {},
+					["core.keybinds"] = {
+						config = {
+							hook = function(keybinds)
+                                -- Edit keymaps here using : https://github.com/nvim-neorg/neorg/wiki/User-Keybinds#setting-up-a-keybind-hook
+								-- Unmaps any Neorg key from the `norg` mode
+								keybinds.unmap("norg", "n", "gtd")
+								keybinds.unmap("norg", "n", keybinds.leader .. "nn")
+
+								-- Equivalent to `keybinds.remap("norg", "n", "<C-Space>, "<cmd>Neorg keybind norg core.qol.todo_items.todo.task_done<CR>")
+								keybinds.remap_event("norg", "n", "<leader>td", "core.qol.todo_items.todo.task_done")
+								keybinds.remap_event("norg", "n", "<leader>tu", "core.qol.todo_items.todo.task_undone")
+								keybinds.remap_event("norg", "n", "<leader>tp", "core.qol.todo_items.todo.task_pending")
+								keybinds.remap_event("norg", "n", "<leader>th", "core.qol.todo_items.todo.task_hold")
+								keybinds.remap_event("norg", "n", "<leader>tc", "core.qol.todo_items.todo.task_cancelled")
+								keybinds.remap_event("norg", "n", "<leader>tr", "core.qol.todo_items.todo.task_recuring")
+								keybinds.remap_event("norg", "n", "<leader>ti", "core.qol.todo_items.todo.task_important")
+								keybinds.remap_event("norg", "n", "<leader>tt", "core.qol.todo_items.todo.task_cycle")
+                                -- Revove any TODO status on the current line:
+								keybinds.map("norg", "n", "<leader>tx", ":lua local line = vim.fn.getline('.'):gsub('%-*(%([^%s]%)%s*', '', 1):gsub('%s*%.%s*', ' ', 1); vim.fn.setline('.', line)<CR>")
+							end,
+						},
+					},
 					["core.completion"] = {
 						config = {
 							engine = "nvim-cmp",
@@ -23,7 +45,8 @@ return {
 					["core.concealer"] = {}, -- TODO : Check folding config here
 					--["core.integrations.image"] = {},
 					--["core.latex.renderer"] = {},
-                    ["core.summary"] = { },
+					["core.integrations.treesitter"] = {},
+					["core.summary"] = {},
 					["core.dirman"] = {
 						config = {
 							workspaces = { notes = "~/Personal/MyNotes/" },
