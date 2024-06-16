51bfe3a129d7262ba9accc95040e525d300528af
Ismaïl ABDEL WAHAB <is.abdelwahab@gmail.com>
2024-06-16 18:36:17 +0200

    [nvim] Updating oil config

--- .config/nvim/lua/plugins/oil-nvim.lua
+++ .config/nvim/lua/plugins/oil-nvim.lua
@@ -1,13 +1,23 @@
 return {
-	"stevearc/oil.nvim",
-	config = function()
-		require("oil").setup({
-			columns = {
-				"icon",
-				"permissions",
-				"size",
-				--"mtime",
-			},
-		})
-	end,
+    "stevearc/oil.nvim",
+    config = function()
+        require("oil").setup({
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+                --"mtime",
+            },
+
+            float = { -- Padding around the floating window
+                padding = 10,
+                max_width = 0,
+                max_height = 0,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                },
+            },
+        })
+    end,
 }
